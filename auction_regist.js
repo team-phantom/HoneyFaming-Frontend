770ad7813440e2728c836cff47c3c8de9324f313
yoonju3221 <40294277+yoonju3221@users.noreply.github.com>
2019-11-26 16:06:16 +0900

    ADD : 경매등록모달 기능 추가

--- auction_regist.js
+++ auction_regist.js
@@ -2,7 +2,6 @@ window.onload = function () {
     var arModal = document.getElementById('arModal');
     var arImageBtn = document.getElementById("arImageBtn");
     var span = document.getElementsByClassName("close")[0];
-    var shippingOption = document.getElementsByClassName("shippingOption");
 
     arImageBtn.onclick = function () {
         arModal.style.display = "block";
@@ -15,9 +14,20 @@ window.onload = function () {
             arModal.style.display = "none";
         }
     }
-    shippingOption.onclick = function(){
-        shippingOption.classList.add('on');
-    }
     
 
+}
+
+$(".shippingOption").click(function(e){
+    var se = $(e.target);
+    console.log(se.children);
+    se.css('color', 'white');
+    se.css('background', '#ffd600');
+
+    $(e.target).siblings().css('color', 'b3b3b3');
+    $(e.target).siblings().css('backgrond', 'white');
+})
+
+function upload(n){
+    document.getElementById('file_upload').value = n;
 }
